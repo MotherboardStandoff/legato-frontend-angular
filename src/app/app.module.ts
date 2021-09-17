b6b04b3d4ebbf7da1b6d03bfb79c95ff55211319
MotherboardStandoff <jeroen.vanhoof87@gmail.com>
2021-09-17 09:25:55 +0200

    project restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { AngularMaterialModule } from './shared/module/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
