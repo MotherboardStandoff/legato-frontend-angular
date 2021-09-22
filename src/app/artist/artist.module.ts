03d8956563e76d0f8a96922b3cf306ec28008959
MotherboardStandoff <jeroen.vanhoof87@gmail.com>
2021-09-22 12:16:57 +0200

    cleanup artist module imports

--- src/app/artist/artist.module.ts
+++ src/app/artist/artist.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ArtistRoutingModule } from './artist-routing.module';
-import { AngularMaterialModule } from '../shared/module/angular-material/angular-material.module';
-import { ArtistComponentsModule } from './component/artist-components.module';
-import { ArtistViewsModule } from './view/artist-views.module';
 import { ArtistComponent } from './artist.component';
 import { SharedComponentsModule } from '../shared/components/shared-components.module';
 
